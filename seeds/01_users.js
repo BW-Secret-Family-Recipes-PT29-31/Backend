aed5a23dbee1f64e0c1e02bee92914db59df25a9
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-10-19 15:45:24 -0700

    trying to resolve merge conflicts

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -6,14 +6,14 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
         {username: 'Austin', password: 'hashed password'},
-<<<<<<< HEAD
+HEAD
 
         {username: 'Danielle', password:'hashed password'},
         {username: 'Constance', password: 'hashed password'},
         {username: 'Richino', password: 'hashed password'},
         {username: 'Sofia', password: 'hashed password'},
-=======
->>>>>>> main
+
+ main
         {username: 'Kendall', password:'hash password'},
         {username: 'Sara', password: 'hashes password'},
         {username: 'Roxanne', password: 'hashs password'},
