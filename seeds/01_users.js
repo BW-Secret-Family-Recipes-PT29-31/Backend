6d760393cac276040a19d6b448a723d39959ed16
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-10-19 15:26:53 -0700

    conflicts

--- seeds/01_users.js
+++ seeds/01_users.js
@@ -6,10 +6,17 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
         {username: 'Austin', password: 'hashed password'},
+<<<<<<< HEAD
         {username: 'Danielle', password:'hashed password'},
         {username: 'Constance', password: 'hashed password'},
         {username: 'Richino', password: 'hashed password'},
         {username: 'Sofia', password: 'hashed password'}
+=======
+        {username: 'Kendall', password:'hash password'},
+        {username: 'Sara', password: 'hashes password'},
+        {username: 'Roxanne', password: 'hashs password'},
+        {username: 'Sofia', password: 'hashd password'}
+>>>>>>> main
       ]);
     });
 };
--- seeds/04_ingredients.js
+++ seeds/04_ingredients.js
@@ -7,39 +7,49 @@ exports.seed = function(knex) {
       return knex('ingredients').insert([
         {
           recipes_id: 1,
-          ingredient: 'potatoes',
-          ingredient: 'onions',
-          ingredient: 'hot peppers',
-          ingredient: 'seasonings',
-          ingredient: 'oil'
+          ingredient: 'Potatoes',
+          ingredient: 'Onions',
+          ingredient: 'Hot peppers',
+          ingredient: 'Seasonings',
+          ingredient: 'Oil'
         },
         {
           recipes_id: 2,
-          ingredient: 'chicken breast',
-          ingredient: 'oil',
-          ingredient: 'seasoning mix',
-          ingredient: 'rice'
+          ingredient: 'Chicken breast',
+          ingredient: 'Oil',
+          ingredient: 'Seasoning mix',
+          ingredient: 'Rice'
         },          
         {
           recipes_id: 3,
-          ingredient: 'steak',
-          ingredient: 'corn tortilla',
-          ingredient: 'onions',
-          ingredient: 'cilantro',
-          ingredient: 'oil',
-          ingredient: 'seasonings'
+          ingredient: 'Steak',
+          ingredient: 'Corn tortilla',
+          ingredient: 'Onions',
+          ingredient: 'Cilantro',
+          ingredient: 'Oil',
+          ingredient: 'Seasonings'
         },
         {
           recipes_id: 4,
-          ingredient:'stuff'
+          ingredient: 'Pasta',
+          ingredient: 'Tomato sauce',
+          ingredient: 'Olive oil',
+          ingredient: 'Pesto',
+          ingredient: 'Seasoned ground beef'
         },
         {
           recipes_id: 5,
-          ingredient: 'things'
+          ingredient: 'Noodles',
+          ingredient: 'Crushed Tomatoes',
+          ingredient: 'Ground Beef',
+          ingredient: 'Onion',
+          ingredient: 'Potatoe'
         },
         {
           recipes_id: 6,
-          ingredient: 'stuff AND things'
+          ingredient: 'Steak',
+          ingredient: 'Shrimp',
+          ingredient: 'Steamed Spinach'
         }      
       ]);
     });
