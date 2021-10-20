38398444b4ae7c625a0bbcbb38b98b1dde5e704c
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-10-19 17:22:41 -0700

    made slight changes to recipes-model, endpoint for getting recipe by ID is still not working

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,41 +1,26 @@
+const express = require("express");
 const router = require("express").Router();
-
 const Recipe = require("./recipes-model");
 //GET ALL RECIPES
-router.get("/api/recipes", (req, res) => {
-  Recipe
-    .find()
-    .then((recipes) => {
-      res.status(200).json(recipes);
-    })
-    .catch((error) => {
-      res
-        .status(401)
-        .json({ error: error, message: "Could not retrieve any recipes" });
-    });
-});
+// router.get("/api/recipes", (req, res) => {
+//   Recipe
+//     .find()
+//     .then((recipes) => {
+//       res.status(200).json(recipes);
+//     })
+//     .catch((error) => {
+//       res
+//         .status(401)
+//         .json({ error: error, message: "Could not retrieve any recipes" });
+//     });
+// });
 
 //GET RECIPE WITH SPECIFIC ID
 
-router.get("api/recipes/recipe_id", (req, res) => {
-  const { id } = req.params.recipe_id;
+router.get("/:recipe_id", async(req, res, next) => {
+  const recipe = await Recipe.findById(req.params.recipe_id)
+  res.status(200).json(recipe)
+})
 
-  Recipe
-    .findById(id)
-    .then((recipe) => {
-      if (recipe) {
-        res.json(recipe_id);
-      } else {
-        res
-          .status(404)
-          .json({ message: "Could not find the recipe with that ID" });
-      }
-    })
-    .catch((error) => {
-      res
-        .status(500)
-        .json({ error: error, message: "Failed to get this recipe" });
-    });
-});
 
 module.exports = router;
