760863a3102812a1372f843c74a922661ef2d8ee
DanielleMathey95 <danielle.mathey@yahoo.com>
2021-10-19 18:31:32 -0700

    changed model and router, trying to implement one at a time to test rather than all at once

--- recipes/recipes-router.js
+++ recipes/recipes-router.js
@@ -1,26 +1,14 @@
 const express = require("express");
 const router = require("express").Router();
 const Recipe = require("./recipes-model");
-//GET ALL RECIPES
-// router.get("/api/recipes", (req, res) => {
-//   Recipe
-//     .find()
-//     .then((recipes) => {
-//       res.status(200).json(recipes);
-//     })
-//     .catch((error) => {
-//       res
-//         .status(401)
-//         .json({ error: error, message: "Could not retrieve any recipes" });
-//     });
-// });
 
 //GET RECIPE WITH SPECIFIC ID
 
-router.get("/:recipe_id", async(req, res, next) => {
-  const recipe = await Recipe.findById(req.params.recipe_id)
-  res.status(200).json(recipe)
-})
+router.get("/:recipe_id", async (req, res, next) => {
+  const recipe = await Recipe.findById(req.params.recipe_id);
+  res.status(200).json(recipe);
+});
 
+//GET INSTRUCTIONS
 
 module.exports = router;
